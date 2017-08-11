517507eb9c5b5f30d1bc09753ceee9b25e82ca8f
ubuntu <huayi.ji910919@gmail.com>
2017-08-11 12:58:19 -0400

    first commit 811

--- server.js
+++ server.js
@@ -10,6 +10,63 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 var mydb;
+var mydb_kilby;
+var kilby_data = [];
+
+// if (typeof localStorage === "undefined" || localStorage === null) {
+//   var LocalStorage = require('node-localstorage').LocalStorage;
+//   localStorage = new LocalStorage('./scratch');
+// }
+ 
+// localStorage.setItem('myFirstKey', 100);
+// console.log(localStorage.getItem('myFirstKey'));
+// localStorage.setItem("test",test);
+// console.log(localStorage.getItem("test"));
+// localStorage.setItem('favoriteflavor','vanilla');  
+
+// var GoogleMapsAPI = require('googlemaps');
+// var gmAPI = new GoogleMapsAPI();
+// var params = {
+//   center: '444 W Main St Lock Haven PA',
+//   zoom: 15,
+//   size: '500x400',
+//   maptype: 'roadmap',
+//   markers: [
+//     {
+//       location: '300 W Main St Lock Haven, PA',
+//       label   : 'A',
+//       color   : 'green',
+//       shadow  : true
+//     },
+//     {
+//       location: '444 W Main St Lock Haven, PA',
+//       icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=cafe%7C996600'
+//     }
+//   ],
+//   style: [
+//     {
+//       feature: 'road',
+//       element: 'all',
+//       rules: {
+//         hue: '0x00ff00'
+//       }
+//     }
+//   ],
+//   path: [
+//     {
+//       color: '0x0000ff',
+//       weight: '5',
+//       points: [
+//         '41.139817,-77.454439',
+//         '41.138621,-77.451596'
+//       ]
+//     }
+//   ]
+// };
+// gmAPI.staticMap(params); // return static map URL 
+// gmAPI.staticMap(params, function(err, binaryImage) {
+//   // fetch asynchronously the binary image 
+// });
 
 /* Endpoint to greet and add a new visitor to database.
 * Send a POST request to localhost:3000/api/visitors with body
@@ -62,6 +119,17 @@ app.get("/api/visitors", function (request, response) {
   });
 });
 
+// var googleMapsClient = require('@google/maps').createClient({
+//   key:  'AIzaSyAjPWQR-VcoynKYktMgVVPHHvWUM-8ZhGs'
+// });
+// // Geocode an address.
+// googleMapsClient.geocode({
+//   address: '1600 Amphitheatre Parkway, Mountain View, CA'
+// }, function(err, response) {
+//   if (!err) {
+//     console.log(response.json.results);
+//   }
+// });
 
 // load local VCAP configuration  and service credentials
 var vcapLocal;
@@ -74,6 +142,8 @@ const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
 
 const appEnv = cfenv.getAppEnv(appEnvOpts);
 
+
+/* insert */
 if (appEnv.services['cloudantNoSQLDB']) {
   // Load the Cloudant library.
   var Cloudant = require('cloudant');
@@ -93,6 +163,96 @@ if (appEnv.services['cloudantNoSQLDB']) {
   // Specify the database we are going to use (mydb)...
   mydb = cloudant.db.use(dbName);
 }
+/*  ******/
+
+
+
+/* read */
+if (appEnv.services['cloudantNoSQLDB']) {
+  // Load the Cloudant library.
+  var Cloudant = require('cloudant');
+
+  // Initialize database with credentials
+  var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+
+  //database name
+  var dbName_kilby = 'kilby_network_07_13_07_17_nobeacon_update_801_csv';
+
+  cloudant.db.list(function(err, allDbs) {
+  console.log('All my databases: %s', allDbs.join(', '))
+});
+  mydb_kilby = cloudant.db.use(dbName_kilby);
+  
+  mydb_kilby.find({selector:{id:'3'}}, function(er, result) {
+  if (er) {
+    throw er;
+  }
+
+  console.log('Found %d documents with id 3', result.docs.length);
+  // for (var i = 0; i < result.docs.length; i++) {
+    for (var i = 0; i < 5; i++) {
+    console.log('  app per: %s', result.docs[i].app_per);
+    // kilby_data.push([result.docs[i].ts, result.docs[i].app_per]);
+  }
+  // console.log("all settttt");
+  // var ts = require("timeseries-analysis");
+  // var t     = new ts.main(ts.adapter.fromArray(kilby_data));
+  // console.log(t);
+  // var forecastDatapoint = 11; 
+// var coeffs = t.ARMaxEntropy({
+//     data: t.data.slice(0,10)
+// });
+// console.log(coeffs);
+// var forecast  = 0;  
+// for (var i=0;i<coeffs.length;i++) { 
+//     forecast -= t.data[10-i][1]*coeffs[i];
+// }
+// console.log("forecast",forecast);
+});
+
+}
+
+/******** forecasting ******/
+// var ts = require("timeseries-analysis");
+// var t       = new ts.main(ts.adapter.sin({cycles:4}));
+// console.log(t);
+// var t     = new ts.main(ts.adapter.fromArray(kilby_data));
+// console.log(t);
+// var forecastDatapoint = 11; 
+// var coeffs = t.ARMaxEntropy({
+//     data: t.data.slice(0,10)
+// });
+// console.log(coeffs);
+// var forecast  = 0;  
+// for (var i=0;i<coeffs.length;i++) { 
+//     forecast -= t.data[10-i][1]*coeffs[i];
+// }
+// console.log("forecast",forecast);
+///////////////////////////////////
+// function generateRandom(){
+//     return Math.random() * (0.06 - 0.00) + 0.00;
+//   }
+//   var data = [generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom()];
+// var timeseries = require("timeseries-analysis");
+//         var t     = new timeseries.main(timeseries.adapter.fromArray(data));
+//         t.ma().lwma();
+//         var processed = t.ma().output();
+//         var chart_url = t.ma({period: 14}).chart();
+//         var min = t.min();
+//         var max = t.max(); 
+//         var mean = t.mean();
+//         var stdev = t.stdev();
+//         var forecastDatapoint   = 7;   
+//         var coeffs = t.ARMaxEntropy({
+//             data:   t.data.slice(0,6)
+//         });
+//         console.log(coeffs);
+//         var forecast    = 0;    // Init the value at 0.
+//         for (var i=0;i<coeffs.length;i++) { // Loop through the coefficients
+//             forecast -= t.data[6-i]*coeffs[i];
+//         }
+//         console.log("forecast",forecast);
+/********/
 
 //serve static file (index.html, images, css)
 app.use(express.static(__dirname + '/views'));
